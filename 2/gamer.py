69d0f7f490829e85cb552b650d257387d4a0843a
Levi Klein Marifosque <levikleinm@gmail.com>
2023-12-02 08:14:05 +0100

    Add helper for 2

--- 2/gamer.py
+++ 2/gamer.py
@@ -1,5 +1,15 @@
 import re
 
+def get_color_number(string, color):
+    re_pattern = f"(\d+) {color}"
+    draw_color = re.search(re_pattern, string) 
+    if draw_color is not None:
+        return int(draw_color.group(1))
+    else:
+        return 0
+
+
+
 f = open("/home/leviklein/repo/advent_of_code_2023/2/input.txt", "r")
 a = f.readline()
 
@@ -8,7 +18,8 @@ max_green = 13
 max_blue = 14
 
 
-total = 0
+total_powers = 0
+total_games = 0
 while a:
     game = re.search("Game (\d+):", a)
     idx = game.end()
@@ -23,43 +34,45 @@ while a:
     game_max_blue = 1
 
     # for draw in draws:
-    #     if re.search("(\d+) red", draw) is not None:
-    #         if int(re.search("(\d+) red", draw).group(1)) > max_red: 
-    #             game_over = True
-    #             break
-    #     if re.search("(\d+) green", draw) is not None:
-    #         if int(re.search("(\d+) green", draw).group(1)) > max_green: 
-    #             game_over = True
-    #             break
-    #     if re.search("(\d+) blue", draw) is not None:
-    #         if int(re.search("(\d+) blue", draw).group(1)) > max_blue: 
-    #             game_over = True
-    #             break
+    #     draw_red = get_color_number(draw, "red")
+    #     if draw_red > max_red: 
+    #         game_over = True
+    #     draw_green = get_color_number(draw, "green")
+    #     if draw_green > max_green: 
+    #         game_over = True
+    #     draw_blue = get_color_number(draw, "blue")
+    #     if draw_blue > max_blue: 
+    #         game_over = True
+
 
     ## problem 2
     for draw in draws:
-        draw_red = re.search("(\d+) red", draw) 
-        if draw_red is not None:
-            draw_red = int(draw_red.group(1))
-            if draw_red > game_max_red: 
-                game_max_red = draw_red
-        draw_green = re.search("(\d+) green", draw) 
-        if draw_green is not None:
-            draw_green = int(draw_green.group(1))
-            if draw_green > game_max_green: 
-                game_max_green = draw_green
-        draw_blue = re.search("(\d+) blue", draw) 
-        if draw_blue is not None:
-            draw_blue = int(draw_blue.group(1))
-            if draw_blue > game_max_blue: 
-                game_max_blue = draw_blue
+        draw_red = get_color_number(draw, "red")
+        if draw_red > game_max_red: 
+            game_max_red = draw_red
+        if draw_red > max_red: 
+            game_over = True
+
+        draw_green = get_color_number(draw, "green")
+        if draw_green > game_max_green: 
+            game_max_green = draw_green
+        if draw_green > max_green: 
+            game_over = True
+
+        draw_blue = get_color_number(draw, "blue")
+        if draw_blue > game_max_blue: 
+            game_max_blue = draw_blue
+        if draw_blue > max_blue: 
+            game_over = True
 
 
-    # if not game_over:
-    #     total += game
+    if not game_over:
+        total_games += game
+
     power = game_max_red * game_max_green * game_max_blue
-    total += power
+    total_powers += power
 
     a = f.readline()
 
-print(total)
+print(total_games)
+print(total_powers)
