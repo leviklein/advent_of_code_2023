98d00d3dfc50963a7539dc0c1c5a771bf2de5503
Levi Klein Marifosque <levikleinm@gmail.com>
2023-12-05 16:11:21 +0100

    add proper solution for day 5 part 1

--- 5/fertilizer.py
+++ 5/fertilizer.py
@@ -1,5 +1,6 @@
 import re
 import pathlib
+import sys
 
 # Global vars
 DATA_TABLE = [] # [seed, soil, fertilizer, water, light, temperature, humidity, location]
@@ -15,34 +16,51 @@ def process_input(file, row):
         source = int(values[1])
         length = int(values[2])
 
-        while(len(table) < source+length-1):
-            table[len(table)] = len(table)
-
-        for i in range(length):
-            table[source+i] = dest+i
-
+        table[source] = {"dest": dest, "len": length}
         row += 1
         line = file[row]
     
     MAPPINGS.append(table)
     return row
 
-def create_data_table():
-    for seed in range(len(MAPPINGS[0])):
-        entry = [seed]
-        for i in MAPPINGS:
-            entry.append(i.get(entry[-1], entry[-1]))
+def get_lowest_index(num, sorted_list):
+    lowest_index = sorted_list[0]
+    for i in sorted_list:
+        if num >= i:
+            lowest_index = i
+            continue
+        break
+    return lowest_index
 
-        DATA_TABLE.append(entry)
+def get_mapping_value(source, table):
+    offset = 0
+    lowest_index = get_lowest_index(source, sorted(table))
+    if source >= lowest_index: 
+        entry = table[lowest_index]
+        if lowest_index + entry["len"] > source:
+            offset = entry["dest"] - lowest_index
+
+    dest = source + offset
+    return dest
     
+def get_mapping_across_tables(source, source_column,dest_column):
+    dest = source
+    for i in range(source_column, dest_column+1):
+        dest = get_mapping_value(dest, MAPPINGS[i])
+    
+    return dest
 
+def get_seed_location(seed):
+    return get_mapping_across_tables(seed, 0, 6)
+    
 
 if __name__ == "__main__":
     script_path = str(pathlib.Path(__file__).parent.resolve()) + "/"
     f = open(script_path + "input.txt", "r")
+    # f = open(script_path + "test_input.txt", "r")
     a = f.read().split('\n')
 
-
+    input = [ int(x) for x in re.findall("\d+", a[0]) ]
     target_seeds = re.findall("\d+", a[0])
 
     row = 1
@@ -51,15 +69,10 @@ if __name__ == "__main__":
             row = process_input(a, row+1)
         row+=1
 
-    create_data_table()
-
-    # print(MAPPINGS)
-    # print(DATA_TABLE)
-
     location_seed = {}
     for seed in target_seeds:
         seed = int(seed)
-        location = DATA_TABLE[seed][-1]
+        location = get_seed_location(seed)
         location_seed[location] = seed
 
     
