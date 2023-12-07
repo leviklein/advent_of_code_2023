4a4d6273b8691dde2f2b64ecc72829b87b1a5cc9
Levi Klein Marifosque <levi.marifosque@asml.com>
2023-12-07 13:01:03 +0100

    Add changes to template

--- template.py
+++ template.py
@@ -1,14 +1,16 @@
 import re
 import pathlib
-script_path = pathlib.Path(__file__).parent.resolve()
 
-f = open(script_path + "input.txt", "r")
-f = open(script_path + "test_input.txt", "r")
-a = f.readline()
+if __name__ == "__main__":
+    script_path = str(pathlib.Path(__file__).parent.resolve()) + "/"
 
-row = 1
-while a:
-
-
+    f = open(script_path + "input.txt", "r")
+    f = open(script_path + "test_input.txt", "r")
     a = f.readline()
-    row += 1
+
+    row = 1
+    while a:
+
+
+        a = f.readline()
+        row += 1
