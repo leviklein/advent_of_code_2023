2502e13ba55c61c6f00a8f5f291355cc475876f3
Levi Klein Marifosque <levikleinm@gmail.com>
2023-12-08 12:43:07 +0100

    add comments

--- 7/camel_cards.py
+++ 7/camel_cards.py
@@ -2,6 +2,7 @@ import pathlib
 from collections import Counter
 
 SUIT_ORDER = ['c', 'd', 'h', 's']
+# from phevaluator
 rank_map = {
     "2": 0, "3": 1, "4": 2, "5": 3, "6": 4, "7": 5, "8": 6, "9": 7,
     "T": 8, "J": 9, "Q": 10, "K": 11, "A": 12,
